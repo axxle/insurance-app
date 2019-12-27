7a0767790d0227708497b75f9e8396c3b322435b
axxle <kvv0911@gmail.com>
2019-12-27 10:43:20 +0500

    Локальная сборка фронта для хероку

---
+++ insurance/src/main/webapp/precache-manifest.6ce1621a0b51d76fd2a53ccc23539a79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934884050ee63175e6aec31396c7b6d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a2c185ddac1d7dfa6df",
+    "url": "/static/css/main.371cd945.chunk.css"
+  },
+  {
+    "revision": "e659564c061d25f89d60",
+    "url": "/static/js/2.cc637848.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.cc637848.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a2c185ddac1d7dfa6df",
+    "url": "/static/js/main.97ac6734.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  }
+]);
